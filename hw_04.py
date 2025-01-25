1d9f5a724501a5173feeeaeb03bbef11a6a7e114
denbmkgit <116366974+denbmkgit@users.noreply.github.com>
2025-01-25 13:36:27 +0300

    hw_2025_04_finish

---
+++ hw_04.py
@@ -0,0 +1,29 @@
+# module 4.3
+
+
+
+
+# # module 4.2
+# def test_function():
+#     def inner_function():
+#         print("Я в области видимости функции test_function")
+#
+#     inner_function()
+#
+#
+# test_function()
+# # inner_function()
+
+
+# # module 4.1
+# from fake_math import divide as fake_divide
+# from true_math import divide as true_divide
+#
+# result1 = fake_divide(69, 3)
+# result2 = fake_divide(3, 0)
+# result3 = true_divide(49, 7)
+# result4 = true_divide(15, 0)
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
